185b3bbe3c2d6334be09d502b9b93bfda238846e
Sven Haider <sven.haider@web.de>
2020-11-17 12:33:11 +0100

    setup form to create idea or problem

--- app/controllers/idea_problems_controller.rb
+++ app/controllers/idea_problems_controller.rb
@@ -1,6 +1,35 @@
 class IdeaProblemsController < ApplicationController
 
     def index
-        @idea_problems = IdeaPoblem.all
+        @idea_problems = IdeaProblem.all
+        @categories = Category.all
+    end
+
+    def new
+        @idea_problem = IdeaProblem.new
+        @category = Category.new
+        # authorize @idea_problem
+    end
+    
+    def create
+        @idea_problem = IdeaProblem.new(idea_problem_params)
+        @idea_problem.user = current_user
+        # authorize @idea_problem
+
+        if @idea_problem.save
+            redirect_to idea_problems_path(params[:id]), notice: "Idea/Problem successfully created."
+        else
+            render :new
+        end
+
+    end
+
+    private
+
+    def set_idea_problem
+    end
+
+    def idea_problem_params
+        params.require(:idea_problem).permit(:title, :description_long, :type_idea, :type_problem)
     end
 end
