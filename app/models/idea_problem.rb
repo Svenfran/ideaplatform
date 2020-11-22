33d40851b277ae6e50098a201ce69f92feeeb987
Sven Haider <sven.haider@web.de>
2020-11-22 13:49:23 +0100

    add cloudinary gem

--- app/models/idea_problem.rb
+++ app/models/idea_problem.rb
@@ -2,7 +2,7 @@ class IdeaProblem < ApplicationRecord
   belongs_to :user
   has_many :idea_problem_categories, dependent: :destroy
   has_many :categories, through: :idea_problem_categories
-  has_many_attached :attachments
+  has_many :attachments
   validates :title, :user_id, :category_ids, presence: true
 
 end
